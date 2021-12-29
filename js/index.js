d49069b0ac0baddfd7242bbe5668a73aac8da941
Matias <matiastoso.developer@gmail.com>
2021-12-28 21:04:55 -0300

    nav responsive

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// nav
+const navBtn = document.getElementById('navBtn');
+navBtn.addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('nav-active') 
+})
