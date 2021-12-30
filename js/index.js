cd183ce180943a713eaa1621a1a9200a70b0cde6
Matias <matiastoso.developer@gmail.com>
2021-12-30 17:03:46 -0300

    Menu burger animado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // nav
-const navBtn = document.getElementById('navBtn');
+const navBtn = document.querySelector('.menu__btn');
 navBtn.addEventListener('click', function() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('nav-active') 
+    const menu = document.querySelector('.menu__btn');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('open');
+    nav.classList.toggle('nav-active')
 })
