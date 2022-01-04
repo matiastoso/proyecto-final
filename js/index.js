d59536cdf610b94f5915d050e0d3f9962326d12a
Matias <matiastoso.developer@gmail.com>
2022-01-04 19:39:23 -0300

    Efecto de escritura

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ navBtn.addEventListener('click', function() {
     menu.classList.toggle('open');
     nav.classList.toggle('nav-active')
 })
+
+// efecto escritura
+const typed  = new Typed('.typed', {
+    strings: ['tu negocio', 'hacer crecer tu marca', 'aumentar las ventas', 'exponer tu arte'],
+    typeSpeed: 40,
+    backSpeed: 50,
+    backDelay: 1000,
+    startDelay: 800,
+    loop: true,
+    loopCount: false,
+})
